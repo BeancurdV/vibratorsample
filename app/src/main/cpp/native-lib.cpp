33f800155a5f4cd5f0efd43a8e5c77fff3916e3e
sharknade <sharknade@163.com>
2024-06-27 15:56:17 +0800

    目标代码

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,21 @@
 #include <jni.h>
-#include <android/binder_ibinder.h>
+#include <string>
+#include "android/binder_manager.h"
 
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_beancurdv_vibrator_1sample_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+
+    AIBinder *bpVibrator = AServiceManager_checkService("vibrator");
+
+    std::string hello;
+    if(bpVibrator) {
+        hello = "bpVibrator ok";
+    } else {
+        hello = "hello";
+    }
+
     return env->NewStringUTF(hello.c_str());
 }
